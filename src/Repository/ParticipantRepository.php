914f44ab29f4215ed6c547226df8f003841ee358
Swanje86 <155452775+Swanje86@users.noreply.github.com>
2024-05-13 14:06:34 +0200

    connexion pseudo/mail

--- src/Repository/ParticipantRepository.php
+++ src/Repository/ParticipantRepository.php
@@ -5,6 +5,7 @@ namespace App\Repository;
 use App\Entity\Participant;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bridge\Doctrine\Security\User\UserLoaderInterface;
 use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
@@ -12,11 +13,25 @@ use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
 /**
  * @extends ServiceEntityRepository<Participant>
  */
-class ParticipantRepository extends ServiceEntityRepository implements PasswordUpgraderInterface
+class ParticipantRepository extends ServiceEntityRepository implements UserLoaderInterface
 {
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Participant::class);
+    }
+
+    public function loadUserByIdentifier(string $pseudoOuMail): ?Participant
+    {
+        $em = $this->getEntityManager();
+
+        return $em->createQuery(
+            'SELECT p
+                FROM App\Entity\Participant p
+                WHERE p.pseudo = :query
+                OR p.mail = :query'
+        )
+            ->setParameter('query', $pseudoOuMail)
+            ->getOneOrNullResult();
     }
 
     /**
